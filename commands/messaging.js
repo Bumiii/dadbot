9f6c3c83b3a69e1b2477308a0b1b020c9db8f56f
Bumi <bumi.iii001@gmail.com>
2021-05-31 17:55:10 -0400

    sutff

--- commands/messaging.js
+++ commands/messaging.js
@@ -1,24 +1,24 @@
-const discord = require("discord.js")
+// const discord = require("discord.js")
 
-module.exports = {
-    name: 'messaging',
-    description: "messages people",
-    async execute(client, message, args, Discord) {
-        setTimeout(function () {
-            message.channel.send("Oh")
-        }, 500)
-        setTimeout(function () {
-            target = message.mentions.users.first();
-            text = message.content.split("> ");
-            user = message.author.username;
-            message.delete();
-            const embed = new discord.MessageEmbed()
-                .setColor('#e5c37c')
-                .setTitle(`Message from ${user}`)
-                .setDescription(text[1]);
-            target.send(embed);
-            message.reply("The message has been sent.");
-            console.log("Command executed");
-        }, 1500)
-    }
-}
+// module.exports = {
+//     name: 'messaging',
+//     description: "messages people",
+//     async execute(client, message, args, Discord) {
+//         setTimeout(function () {
+//             message.channel.send("Oh")
+//         }, 500)
+//         setTimeout(function () {
+//             target = message.mentions.users.first();
+//             text = message.content.split("> ");
+//             user = message.author.username;
+//             message.delete();
+//             const embed = new discord.MessageEmbed()
+//                 .setColor('#e5c37c')
+//                 .setTitle(`Message from ${user}`)
+//                 .setDescription(text[1]);
+//             target.send(embed);
+//             message.reply("The message has been sent.");
+//             console.log("Command executed");
+//         }, 1500)
+//     }
+// }
